24334d05c8888897087a1044cf154d0f325878e6
Jan Blunck <jblunck@vyatta.com>
2013-09-02 16:56:39 +0200

    Use PERSISTENCE_PATH with a trailing "/"
    When using PERSISTENCE_PATH we should prepend and append a pathname
    separator as this is done with other user provided paths as well, e.g.
    LIVE_MEDIA_PATH.
    
    Signed-off-by: Jan Blunck <jblunck@vyatta.com>

--- scripts/boot/9990-misc-helpers.sh
+++ scripts/boot/9990-misc-helpers.sh
@@ -895,7 +895,7 @@ probe_for_file_name ()
 
 	for label in ${overlays}
 	do
-		path=${backing}/${PERSISTENCE_PATH}${label}
+		path=${backing}/${PERSISTENCE_PATH}/${label}
 		if [ -f "${path}" ]
 		then
 			local loopdev
